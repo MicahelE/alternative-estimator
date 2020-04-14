48b07defc43b6a2b597aa53931c90edf34cf235d
unknown <imohgenius@yahoo.com>
2020-04-14 10:25:41 +0100

    ch4

--- src/estimator.php
+++ src/estimator.php
@@ -1,10 +1,13 @@
 <?php
 
+
+
 $data= array( "region"=> array("name"=>"Africa", "avgAge"=>19.7, "avgDailyIncomeInUSD"=>5, "avgDailyIncomePopulation"=>0.71),"periodType"=> "days",
 "timeToElapse"=> 58,
 "reportedCases"=> 674,
 "population"=> 66622705,
 "totalHospitalBeds"=> 1380614);
+
 
 
 function periodType($data)
@@ -22,14 +25,33 @@ function periodType($data)
 }
 }
 
+function periodDay($data)
+{
+  switch ($data["periodType"]) {
+    case "days":
+        return $data["timeToElapse"];
+    break;
+    case "weeks":
+        return $data["timeToElapse"]*7;
+    break;
+    case "months":
+        return $data["timeToElapse"]*30;
+    break;
+}
+}
+
 function covid19ImpactEstimator($data)
 {
-  $impact["currentlyInfected"]=$data["reportedCases"]*10;
 
-  $severeImpact["currentlyInfected"]=$data["reportedCases"]*50;
+  $factor1=10;
+
+$factor2=50;
+  $impact["currentlyInfected"]=$data["reportedCases"]*$factor1;
+
+  $severeImpact["currentlyInfected"]=$data["reportedCases"]*$factor2;
 
   $impact["infectionsByRequestedTime"]=$impact["currentlyInfected"] * (pow(2,periodType($data)));
-  ;
+  
   $severeImpact["infectionsByRequestedTime"]=$severeImpact["currentlyInfected"]*pow(2,periodType($data));
 
   $impact["severeCasesByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.15;
@@ -38,24 +60,108 @@ function covid19ImpactEstimator($data)
   $impact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35)-$impact["severeCasesByRequestedTime"]);
   $severeImpact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35)-$severeImpact["severeCasesByRequestedTime"]);
   $impact["casesForICUByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.05;
-  $severeImpact["casesForICUByRequestedTime"]=$impact["infectionsByRequestedTime"]*0.05;
+  $severeImpact["casesForICUByRequestedTime"]=$severeImpact["infectionsByRequestedTime"]*0.05;
   $impact["casesForVentilatorsByRequestedTime"]=intval($impact["infectionsByRequestedTime"]*0.02);
-  $severeImpact["casesForVentilatorsByRequestedTime"]=intval($impact["infectionsByRequestedTime"]*0.02);
+  $severeImpact["casesForVentilatorsByRequestedTime"]=intval($severeImpact["infectionsByRequestedTime"]*0.02);
 
-  $impact["dollarsInFlight"]=$impact["infectionsByRequestedTime"]*0.65*1.5*30;
-  $severeImpact["dollarsInFlight"]=($severeImpact["infectionsByRequestedTime"]*0.65*1.5*30);
+  $impact["dollarsInFlight"]=intval(($impact["infectionsByRequestedTime"]*$data["region"]["avgDailyIncomePopulation"]*$data["region"]["avgDailyIncomeInUSD"])/periodDay($data));
+  $severeImpact["dollarsInFlight"]=intval(($severeImpact["infectionsByRequestedTime"]*$data["region"]["avgDailyIncomePopulation"]*$data["region"]["avgDailyIncomeInUSD"])/periodDay($data));
 
   $output= array("data"=>$data,"impact"=>$impact,"severeImpact"=>$severeImpact);
  
   
 
 
-  // die(var_dump($output));
-  return $output;
+  // die(var_dump($output,periodDay($data)));
+   return $output;
  
 
 }
 
 covid19ImpactEstimator($data);
+
+//BACKEND WORK
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri_path 			= array_filter(explode( '/', $uri ));
+$content_type 	= end($uri_path);
+$requestMethod 	= $_SERVER["REQUEST_METHOD"];
+
+
+$time_pre_exec = microtime(true);
+if ( $content_type == 'xml' ) {
+
+	header("Content-Type: application/xml; charset=UTF-8");
+	$input = trim(file_get_contents("PHP://input"));
+	$input = json_decode($input, true);
+
+	$estimate = covid19ImpactEstimator($input);
+	$xml 			= new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><data/>');
+	to_xml($xml, $estimate);
+	$result = $xml->asXML();
+
+	header("HTTP/1.1 200 OK");
+	echo $result;
+	
+} elseif ( $content_type == 'logs' ) {
+
+	header("Content-Type: text/plain; charset=UTF-8");
+	$file = "logs.txt";
+	$logs = file_get_contents($file);
+
+	header("HTTP/1.1 200 OK");
+	echo $logs;
+
+} else {
+
+	header("Content-Type: application/json; charset=UTF-8");
+	$input = trim(file_get_contents("PHP://input"));
+	$input = json_decode($input, true);
+
+	$estimate	= covid19ImpactEstimator($input);
+	header("HTTP/1.1 200 OK");
+	$response = json_encode($estimate);
+	echo $response;
+}
+
+logRequest( $requestMethod, $uri, $time_pre_exec );
+
+function to_xml(SimpleXMLElement $object, array $data) {   
+
+  foreach ($data as $key => $value) {
+      if (is_array($value)) {
+          $new_object = $object->addChild($key);
+          to_xml($new_object, $value);
+      } else {
+          // if the key is an integer, it needs text with it to actually work.
+          if ($key == (int) $key) {
+              $key = "$key";
+          }
+
+          $object->addChild($key, $value);
+      }   
+  }   
+} 
+
+function logRequest( $requestMethod, $uri, $time_pre_exec ) {
+
+	$responseCode = http_response_code(); 
+	$time_post_exec = microtime(true);
+	$exec_time 			= $time_post_exec - $time_pre_exec;
+	$exec_time 			= strtok($exec_time, ".");
+	$length 				= strlen($exec_time);
+
+	if ($length < 2) {
+		$exec_time = '0' . $exec_time;
+	}
+
+	$log_txt = "$requestMethod" . "\t\t" . "$uri" .  "\t\t" . $responseCode . "\t\t" . $exec_time . 'ms';
+	file_put_contents( 'logs.txt', $log_txt.PHP_EOL , FILE_APPEND );
+}
 
 ?>
